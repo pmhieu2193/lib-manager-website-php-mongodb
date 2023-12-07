b9ecc111b763294510f7de8f5065a8d6beeb1285
BigGok <116859641+BigGok@users.noreply.github.com>
2023-12-07 21:55:23 +0700

    Tai Update nav.php

--- nav.php
+++ nav.php
@@ -1,16 +1,15 @@
 <?php
     include ("connection.php");
-    session_start(); 
 ?>
-
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <?php echo'
     <div class="nav">
         <img src="img/dark-logo.png" class="brand-logo" alt="">
         <div class="nav-items">
-                <div class="search">
-                    <input type="text" class="search-box" placeholder="Tìm tên thương hiệu, sản phẩm...">
-                    <button class="search-btn">Tìm kiếm</button>                       
-                </div>
+        <form action="search.php" method="GET" accept-charset="UTF-8">
+            <input type="text"  name="timkiem" placeholder="Tìm tên thương hiệu, sản phẩm...">
+            <input type="submit" value="Tìm kiếm" class="search-btn"> 
+        </form>
                 <a>
                     <img src="img/user.png" id="user-img" alt="">
                     <div class="login-logout-popup hide">
@@ -35,12 +34,44 @@
         <li class="link-item"><a href="index.html" class="link"></a>Nội quy</li> 
         <li class="link-item"><a href="order.html" class="link"></a>Hướng dẫn sử dụng</li> 
         <li class="link-item"> 
-            <div>
-                <select name="mySelect" class="link">
-                <option value="">Thể loại</option>'; ?>
-                <?php $collection = $database->selectCollection('the_loai');
-                $result = $collection->find([]);
-                foreach ($result as $document) {
-                    echo '<option value="index.php?';?><?php echo $document->ma_the_loai;?><?php echo'">';?> <?php echo $document->ten_the_loai;?> <?php echo'</option>'; ?>
-            <?php } ?>
-    <?php echo '</select> </div> </li> </ul>'; ?>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Thể loại
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">'; ?>
+        <?php 
+            $collectionSach = $database->selectCollection('sach');
+            $collectionTheLoai = $database->selectCollection('the_loai');
+            $aggregateQuery = [
+                [
+                    '$lookup' => [
+                        'from' => 'the_loai',
+                        'localField' => 'ma_the_loai',
+                        'foreignField' => 'ma_the_loai',
+                        'as' => 'the_loai'
+                    ]
+                ],
+                [
+                    '$unwind' => '$the_loai'
+                ],
+                [
+                    '$group' => [
+                        '_id' => '$ma_the_loai',
+                        'ten_the_loai' => ['$first' => '$the_loai.ten_the_loai']
+                    ]
+                ]
+            ];
+            
+            $result = $collectionSach->aggregate($aggregateQuery);
+            
+            foreach ($result as $document) {
+                $maTheLoai = $document->_id;
+                $tenTheLoai = $document->ten_the_loai;
+                echo '<a class="dropdown-item" href="search.php?ma_the_loai=' . $maTheLoai . '">' . $tenTheLoai . '</a>';
+            }
+        ?>
+
+    <?php echo '</div> </div> </li> </ul>'; ?>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
